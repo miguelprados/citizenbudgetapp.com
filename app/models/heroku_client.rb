146c39fc605d8d2d9a55270e2fc2e334fd793d3b
James McKinney <james@slashpoundbang.com>
2014-05-08 23:18:01 -0400

    parse heroku json differently

--- app/models/heroku_client.rb
+++ app/models/heroku_client.rb
@@ -8,14 +8,14 @@ class HerokuClient # avoid clash with heroku gem
     # Lists the domain names associated to the Heroku app.
     # @return [Array<String>] a list of domain names
     def list_domains
-      client.get("/apps/#{ENV['HEROKU_APP']}/domains").body.map{|domain| domain['domain']}
+      JSON.parse(client.get("/apps/#{ENV['HEROKU_APP']}/domains").body).map{|domain| domain['domain']}
     end
 
     # Adds a domain name to the Heroku app.
     # @param [String] domain a domain name
     # @return [Boolean] whether domain was added
     def add_domain(domain)
-      client.post("/apps/#{ENV['HEROKU_APP']}/domains", domain_name: {domain: domain}).body['domain'] == domain
+      JSON.parse(client.post("/apps/#{ENV['HEROKU_APP']}/domains", domain_name: {domain: domain}).body)['domain'] == domain
     end
 
     # Removes a domain name from the Heroku app.
@@ -23,7 +23,7 @@ class HerokuClient # avoid clash with heroku gem
     # @return [Boolean] whether domain was removed
     def remove_domain(domain)
       begin
-        client.delete("/apps/#{ENV['HEROKU_APP']}/domains/#{domain}").body == {}
+        JSON.parse(client.delete("/apps/#{ENV['HEROKU_APP']}/domains/#{domain}").body) == {}
       rescue MultiJson::DecodeError
         false
       end
@@ -36,7 +36,6 @@ class HerokuClient # avoid clash with heroku gem
       Faraday.new 'https://api.heroku.com', headers: {'Accept' => 'application/json'} do |builder|
         builder.request :url_encoded
         builder.request :basic_auth, nil, ENV['HEROKU_API_KEY']
-        builder.response :json
         builder.adapter :net_http
       end
     end
